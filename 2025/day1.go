7410912a195388898bdcf8f0fdc3480a930eb096
Daniel Machen <dmachen@merlintree.xyz>
2025-12-01 22:50:09 +0000

    Clean up

--- 2025/day1.go
+++ 2025/day1.go
@@ -10,8 +10,8 @@ func day1part1(inputData string) int {
 	var countZero int
 	position := 50
 	rotations := strings.Fields(inputData)
-	for _, rot := range rotations {
 
+	for _, rot := range rotations {
 		direction := rot[:1]
 		distance, _ := strconv.Atoi(rot[1:])
 		distance = distance % 100
@@ -35,35 +35,37 @@ func day1part2(inputData string) int {
 	var countZero int
 	position := 50
 	rotations := strings.Fields(inputData)
+
 	for _, rot := range rotations {
 		direction := rot[:1]
 		distance, _ := strconv.Atoi(rot[1:])
 		fullRotations := int(math.Abs(math.Floor(float64(distance / 100))))
-
 		distance = distance % 100
-		// Rotating right
+
+		// All full rotations pass zero
+		countZero += fullRotations
+
 		if direction == "R" {
-			// Partial right rotation passes zero
+			// Partial right rotation passing zero
 			if (position + distance) > 100 {
-				countZero += 1
+				countZero++
 			}
 			position = (position + distance) % 100
 		}
-		// Rotating left
+
 		if direction == "L" {
-			// Partial left rotation passes zero
+			// Partial left rotation passing zero
 			if distance > position {
 				if position != 0 {
-					countZero += 1
+					countZero++
 				}
 				position = (position - distance) + 100
 			} else {
 				position = (position - distance)
 			}
 		}
-		// Count full rotations
-		countZero += fullRotations
-		// Finally count zero if turning the dial lands on zero
+
+		// Landing on zero
 		if position == 0 {
 			countZero++
 		}
